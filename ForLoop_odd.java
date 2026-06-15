6d663e91edc3a03a43a28698121244f04f9d674e
amutueiretracy-gif <amutueiretracy@gmail.com>
2026-06-15 12:48:31 +0300

    Add files via upload

---
+++ ForLoop_odd.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tracie.loops.forloop_odd;
+
+
+public class ForLoop_odd {
+
+    public static void main(String[] args) {
+       
+        
+        String [] names ={"Julius", "Mutebi", "Asha", "Gonza", "Aboko", "Zamora Bobby",};
+        
+            
+        for(int x =0; x< names.length;x++) {
+              System.out.println(" The name at index :" +x +" is " +names[x]);
+              
+          }
+    }     
+    
+    
+}
